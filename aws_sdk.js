058dd9b018750dc5b2d85fcf9613f3d09a55a899
NancyZhang <Nancy.Zhang@fisherpaykel.com>
2023-01-17 12:19:11 +1300

    add aws_sdk sample

--- aws_sdk.js
+++ aws_sdk.js
@@ -16,29 +16,29 @@ nodes3_createbucket.js
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// // snippet-start:[s3.JavaScript.buckets.createBucketV3]
-// // Get service clients module and commands using ES6 syntax.
-// import { CreateBucketCommand } from "@aws-sdk/client-s3";
-// import { s3Client } from "./libs/s3Client.js";
+// snippet-start:[s3.JavaScript.buckets.createBucketV3]
+// Get service clients module and commands using ES6 syntax.
+import { CreateBucketCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "./libs/s3Client.js";
 
-// // Set the bucket parameters.
+// Set the bucket parameters.
 
-// export const bucketParams = { Bucket: "nancysdkbucket" };
+export const bucketParams = { Bucket: "nancysdkbucket1" };
 
-// // Create the Amazon S3 bucket.
-// export const run = async () => {
-//   try {
-//     const data = await s3Client.send(new CreateBucketCommand(bucketParams));
-//     console.log("Success", data);
-//     return data; // For unit tests.
-//   } catch (err) {
-//     console.log("Error", err);
-//   }
-// };
-// run();
-// // snippet-end:[s3.JavaScript.buckets.createBucketV3]
-// // For unit tests only.
-// // module.exports ={run, bucketParams};
+// Create the Amazon S3 bucket.
+export const run = async () => {
+  try {
+    const data = await s3Client.send(new CreateBucketCommand(bucketParams));
+    console.log("Success", data);
+    return data; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+run();
+// snippet-end:[s3.JavaScript.buckets.createBucketV3]
+// For unit tests only.
+// module.exports ={run, bucketParams};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
