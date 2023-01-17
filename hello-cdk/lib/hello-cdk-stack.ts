2a185266c4adb3719ea672d42b3cb1216ead7253
NancyZhang <Nancy.Zhang@fisherpaykel.com>
2023-01-17 17:07:54 +1300

    add git ignore

---
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    new s3.Bucket(this, 'nancycdkbucket', {
+      versioned: true
+    });
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
