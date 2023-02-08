b0120de88887300c46f77134b9b463cb2bdf38cc
NancyZhang <Nancy.Zhang@fisherpaykel.com>
2023-02-09 11:18:27 +1300

    add code to set region

--- aws_sdk.js
+++ aws_sdk.js
@@ -16,49 +16,56 @@ nodes3_createbucket.js
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// // snippet-start:[s3.JavaScript.buckets.createBucketV3]
-// // Get service clients module and commands using ES6 syntax.
-// import { CreateBucketCommand } from "@aws-sdk/client-s3";
+// snippet-start:[s3.JavaScript.buckets.createBucketV3]
+// Get service clients module and commands using ES6 syntax.
+import { CreateBucketCommand } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 // import { s3Client } from "./libs/s3Client.js";
 
-// // Set the bucket parameters.
+// Set the AWS Region.
+const REGION = "ap-southeast-2";
+// Create an Amazon S3 service client object.
+const s3Client = new S3Client({ region: REGION });
+export { s3Client };
 
-export const bucketParams = { Bucket: "nancysdkbucket0208" };
+// Set the bucket parameters.
 
-// // Create the Amazon S3 bucket.
-// export const run = async () => {
-//   try {
-//     const data = await s3Client.send(new CreateBucketCommand(bucketParams));
-//     console.log("Success", data);
-//     return data; // For unit tests.
-//   } catch (err) {
-//     console.log("Error", err);
-//   }
-// };
-// run();
-// // snippet-end:[s3.JavaScript.buckets.createBucketV3]
-// // For unit tests only.
-// // module.exports ={run, bucketParams};
+export const bucketParams = { Bucket: "logshipper-230209a" };
+
+// Create the Amazon S3 bucket.
+export const run = async () => {
+  try {
+    const data = await s3Client.send(new CreateBucketCommand(bucketParams));
+    console.log("Success", data);
+    return data; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+run();
+// snippet-end:[s3.JavaScript.buckets.createBucketV3]
+// For unit tests only.
+// module.exports ={run, bucketParams};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const AWS = require('aws-sdk');
-// Set the region
-// AWS.config.update({region: 'REGION'});
-// Create S3 service object
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
-const bucketParams = {
-   Bucket: 'nancyrequireawssdk'
-//    CreateBucketConfiguration: {
-//       LocationConstraint: 'REGION'
-//    }
-};
+// const AWS = require('aws-sdk');
+// // Set the region
+// // AWS.config.update({region: 'REGION'});
+// // Create S3 service object
+// const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+// const bucketParams = {
+//    Bucket: 'nancyrequireawssdk'
+// //    CreateBucketConfiguration: {
+// //       LocationConstraint: 'REGION'
+// //    }
+// };
 
-// Create the bucket
-s3.createBucket(bucketParams, function(err, data) {
-   if (err) {
-      console.log("Error", err);
-   } else {
-      console.log("Success", data.Location);
-   }
-});
+// // Create the bucket
+// s3.createBucket(bucketParams, function(err, data) {
+//    if (err) {
+//       console.log("Error", err);
+//    } else {
+//       console.log("Success", data.Location);
+//    }
+// });
